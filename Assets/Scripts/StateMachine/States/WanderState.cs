fc58544723d5373eacd3453ad48ffe00509fa452
Bigtuna709 <robertday7@hotmail.com>
2022-08-19 14:09:07 -0400

    Improved AI behavior in the WanderState

--- Assets/Scripts/StateMachine/States/WanderState.cs
+++ Assets/Scripts/StateMachine/States/WanderState.cs
@@ -3,24 +3,30 @@ using System;
 
 public class WanderState : BaseState
 {
+    private float idleTime = 1f;
     private Vector3? _destination;
     private ZombieController _zombie;
     public WanderState(ZombieController zombie) : base(zombie.gameObject)
     {
         _zombie = zombie;
     }
+    private bool IsZombieIdle() => Time.time >= idleTime;
     public override Type Tick()
     {
         var chasePlayer = CheckForAggro();
-        if(chasePlayer != null)
+        if (chasePlayer != null)
         {
             _zombie.SetTarget(chasePlayer);
             return typeof(ChaseState);
         }
+
         if (_destination.HasValue == false || Vector3.Distance(transform.position, _destination.Value)
             <= _zombie._navMeshAgent.stoppingDistance)
         {
-            FindRandomDestination();
+            if (IsZombieIdle())
+            {
+                FindRandomDestination();
+            }
         }
         return null;
     }
@@ -44,6 +50,8 @@ public class WanderState : BaseState
                     return player.transform;
                 }
             }
+            else
+                Debug.DrawRay(pos, direction * ZombieSettings.ZombieAggroDistance, Color.blue);
 
             direction = angleStep * direction;
         }
@@ -53,35 +61,33 @@ public class WanderState : BaseState
     private void FindRandomDestination()
     {
         RaycastHit hit;
-        var angle = transform.rotation * startingAngle;
-        var direction = angle * Vector3.forward;
         var pos = transform.position;
-        Debug.DrawRay(pos, direction * ZombieSettings.ZombieAggroDistance, Color.blue);
-        if (Physics.Raycast(pos, direction, out hit, ZombieSettings.ZombieAggroDistance))
+
+        Debug.DrawRay(pos, transform.forward * 5f, Color.blue);
+        if (Physics.Raycast(pos, transform.forward, out hit, 5f))
         {
             Debug.Log("Wall");
             if (hit.collider.CompareTag("Wall"))
             {
-                Vector3 newPosition = (transform.position + (-transform.forward * 2f))
-                    + new Vector3(UnityEngine.Random.Range(-2.5f, 2.5f), 0f, UnityEngine.Random.Range(-2.5f, 2.5f));
-                _destination = new Vector3(newPosition.x, 1f, newPosition.z);
-                _zombie._navMeshAgent.SetDestination(_destination.Value);
+                GetRandomPosition(-transform.forward);
             }
             else
             {
-                Vector3 newPosition = (transform.position + (transform.forward * 2f))
-                    + new Vector3(UnityEngine.Random.Range(-2.5f, 2.5f), 0f, UnityEngine.Random.Range(-2.5f, 2.5f));
-                _destination = new Vector3(newPosition.x, 1f, newPosition.z);
-                _zombie._navMeshAgent.SetDestination(_destination.Value);
+                GetRandomPosition(transform.forward);
             }
         }
         else
         {
-            Vector3 newPosition = (transform.position + (transform.forward * 2f))
-                + new Vector3(UnityEngine.Random.Range(-2.5f, 2.5f), 0f, UnityEngine.Random.Range(-2.5f, 2.5f));
-            _destination = new Vector3(newPosition.x, 1f, newPosition.z);
-            _zombie._navMeshAgent.SetDestination(_destination.Value);
+            GetRandomPosition(transform.forward);
         }
-    }  
+    }
 
+    void GetRandomPosition(Vector3 newLocationDirection)
+    {
+        Vector3 newPosition = (transform.position + (newLocationDirection * 2f))
+            + new Vector3(UnityEngine.Random.Range(-1.5f, 1.5f), 0f, UnityEngine.Random.Range(-1.5f, 1.5f));
+        _destination = new Vector3(newPosition.x, 1f, newPosition.z);
+        _zombie._navMeshAgent.SetDestination(_destination.Value);
+        idleTime = Time.time + UnityEngine.Random.Range(0.5f, 2f);
+    }
 }
